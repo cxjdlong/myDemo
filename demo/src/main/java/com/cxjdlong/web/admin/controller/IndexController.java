d2d3dfe536262dcdde0f60e574ad833fd09e1c73
root <root@CentOS6.(none)>
2015-09-10 14:44:18 +0800

    添加后台login地址

--- demo/src/main/java/com/cxjdlong/web/admin/controller/IndexController.java
+++ demo/src/main/java/com/cxjdlong/web/admin/controller/IndexController.java
@@ -1,5 +1,16 @@
 package com.cxjdlong.web.admin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
 public class IndexController {
 
+	@RequestMapping(value="/login")
+	public String login(){
+		System.out.println("test");
+		
+		return "admin/index";
+	}
 }
